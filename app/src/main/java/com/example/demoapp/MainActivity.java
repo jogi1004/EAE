2bc93ce432eea3171784fb92ef631c9e1f1675ac
noeh1001 <noah.ehlert2003@gmail.com>
2023-09-29 14:46:03 +0200

    Homescreen V.2 + Rezeptliste V.1 /Noah

--- app/src/main/java/com/example/demoapp/MainActivity.java
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.demoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ConstraintLayout recipes;
+    ConstraintLayout shoppingBag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipes = findViewById(R.id.recipes);
+        recipes.setOnClickListener(this);
+
+        shoppingBag = findViewById(R.id.shoppingBag);
+        shoppingBag.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == recipes){
+            Intent recipeIntent = new Intent(this, RecipeActivity.class);
+            startActivity(recipeIntent);
+        }
+        if (view == shoppingBag){
+
+        }
+
     }
 }
