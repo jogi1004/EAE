6fd111f691f286194366279ef2b653086fa492dd
jogi1004 <128365438+jogi1004@users.noreply.github.com>
2023-10-03 20:59:03 +0200

    EAE2-13: fehlende DB-Funktionen ergaenzen

--- app/src/main/java/com/example/eaeprojekt/MainActivity.java
+++ app/src/main/java/com/example/eaeprojekt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.eaeprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,17 +17,45 @@ public class MainActivity extends AppCompatActivity {
 
         // Beispieldaten hinzufügen
         DatabaseManager dbMan = new DatabaseManager(this);
-        Log.d("HSKL", "Holen ging noch");
         dbMan.open();
-        String zutatName = "Mehl";
-        String zutatEinheit = "g";
-        dbMan.insertIngredient(zutatName, zutatEinheit);
+        String zutatName1 = "Mehl";
+        String zutatName2 = "Milch";
+        String zutatEinheit1 = "g";
+        String zutatEinheit2 = "ml";
+        dbMan.insertIngredient(zutatName1, zutatEinheit1);
+        dbMan.insertIngredient(zutatName2, zutatEinheit2);
+        logAllIngredients(dbMan);
 
 
         // Datenbank löschen
-        // deleteDatabase(DatabaseManager.DATABASE_NAME);
+         deleteDatabase(DatabaseManager.DATABASE_NAME);
 
         // Optional: Schließe die Datenbankverbindung
         dbMan.close();
+    }
+
+    public void logAllIngredients(DatabaseManager dbMan) {
+        // Hole alle Zutaten aus der Datenbank
+        Cursor cursor = dbMan.getAllIngredients();
+
+        // Überprüfe, ob der Cursor Daten enthält
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                // Holen Sie sich die Zutatennamen und Einheiten aus dem Cursor
+                int nameIndex = cursor.getColumnIndex(DatabaseManager.COLUMN_INGREDIENT_NAME);
+                int unitIndex = cursor.getColumnIndex(DatabaseManager.COLUMN_INGREDIENT_UNIT);
+
+                if (nameIndex != -1 && unitIndex != -1) {
+                    String name = cursor.getString(nameIndex);
+                    String unit = cursor.getString(unitIndex);
+
+                    // Gib die Zutat als Log-Ausgabe aus
+                    Log.d("HSKL", "Name: " + name + ", Einheit: " + unit);
+                }
+            } while (cursor.moveToNext());
+
+            // Cursor schließen, nachdem er nicht mehr benötigt wird
+            cursor.close();
+        }
     }
 }
