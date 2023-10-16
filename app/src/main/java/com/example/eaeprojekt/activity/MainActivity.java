b5d4d47062a5cb2f46843ec259c85980359c1abb
Andre Bieber <anbi1001@hs-kl.de>
2023-10-16 11:54:35 +0200

    V1.0 der Rezeptliste, einfügen der Navigationsleiste

--- app/src/main/java/com/example/eaeprojekt/activity/MainActivity.java
+++ app/src/main/java/com/example/eaeprojekt/activity/MainActivity.java
@@ -1,28 +1,38 @@
 package com.example.eaeprojekt.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import static com.example.eaeprojekt.R.id.AddButtonNavBar;
+import static com.example.eaeprojekt.R.id.recipeListButtonNavBar;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.eaeprojekt.IngredientDTO;
 import com.example.eaeprojekt.R;
 import com.example.eaeprojekt.database.DatabaseManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ConstraintLayout recipes;
-    ConstraintLayout shoppingBag;
+    ImageButton recipe;
+    ImageButton shopping;
+    ImageButton addRecipe;
+    BottomNavigationView b;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_navbar);
 
         // Beispieldaten hinzufügen
         DatabaseManager dbMan = new DatabaseManager(this);
@@ -33,39 +43,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String zutatName2 = "Milch";
         String zutatEinheit1 = "g";
         String zutatEinheit2 = "ml";
-        //dbMan.insertIngredient(zutatName1, zutatEinheit1);
-        //dbMan.insertIngredient(zutatName2, zutatEinheit2);
-        //dbMan.insertRecipe("Pizza", 4, "30", 1);
-        //dbMan.insertRecipe("Pasta", 2, "60", 0);
+        dbMan.insertIngredient(zutatName1, zutatEinheit1);
+        dbMan.insertIngredient(zutatName2, zutatEinheit2);
+        dbMan.insertRecipe("Pizza", 4, "30", 1);
+        dbMan.insertRecipe("Pasta", 2, "60", 0);
         logAllIngredients(dbMan);
 
 
         // Optional: Schließe die Datenbankverbindung
         dbMan.close();
 
-        recipes = findViewById(R.id.recipes);
-        recipes.setOnClickListener(this);
+        b = findViewById(R.id.bottomNavView);
+        b.setSelectedItemId(R.id.AddButtonNavBar);
+        b.setOnItemSelectedListener(this::onNavigationItemSelected);
+        //Verknüpfen der ImageButtons die in der XML eine ID erhalten haben
+        //recipe = findViewById(R.id.recipeListButtonNavBar);
+        //shopping = findViewById(R.id.shoppingBagButtonNavBar);
+        //addRecipe = findViewById(R.id.AddButtonNavBar);
+        //On Click Listener Hinzufügen um das antippen erkennen zu könnens
+        //recipe.setOnClickListener(this);
+        //shopping.setOnClickListener(this);
+        //addRecipe.setOnClickListener(this);
 
-        shoppingBag = findViewById(R.id.shoppingBag);
-        shoppingBag.setOnClickListener(this);
     }
 
     public void logAllIngredients(DatabaseManager dbMan) {
-     List<IngredientDTO> ingredients = dbMan.getAllIngredients();
-     for (IngredientDTO ingredient : ingredients) {
-         Log.d("HSKL", "Name: " + ingredient.getName() + ", Einheit: " + ingredient.getUnit());
-     }
+        List<IngredientDTO> ingredients = dbMan.getAllIngredients();
+        for (IngredientDTO ingredient : ingredients) {
+            Log.d("HSKL", "Name: " + ingredient.getName() + ", Einheit: " + ingredient.getUnit());
+        }
 
     }
 
     @Override
     public void onClick(View view) {
-        if(view == recipes){
+        if (view == recipe) {
             Intent recipeIntent = new Intent(this, RecipeActivity.class);
             startActivity(recipeIntent);
+            finish();
         }
-        if (view == shoppingBag){
-
+        if (view == shopping) {
+            //Öffnen der Einkaufsliste
+        }
+        if (view == addRecipe) {
+            //Öffnen der Hinzufügen Activity
         }
 
     }
@@ -74,4 +95,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onPointerCaptureChanged(boolean hasCapture) {
         super.onPointerCaptureChanged(hasCapture);
     }
+
+    private boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.AddButtonNavBar) {
+            //Öffne ADDActivity
+        }
+        if (id == R.id.recipeListButtonNavBar) {
+            /**
+             Erstellen eines Intents zum Öffnen der RecipeActivity
+            sobald in der Navbar der entsprechende Button gedrückt wurde
+             **/
+            Intent i = new Intent(this, RecipeActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.shoppingBagButtonNavBar) {
+            //Öffnen der Einkaufsliste
+
+        }
+        return false;
+    }
 }
+
