390a4abada9dee5f291109b7d1dc2de264d4179e
noeh1001 <noeh1001@stud.hs-kl.de>
2023-10-20 10:27:41 +0200

    Zutaten werden in db geschrieben

--- app/src/main/java/com/example/eaeprojekt/PopupIngredients.java
+++ app/src/main/java/com/example/eaeprojekt/PopupIngredients.java
@@ -1,22 +1,27 @@
 package com.example.eaeprojekt;
 
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.eaeprojekt.activity.NewRecipeActivity;
 import com.example.eaeprojekt.database.DatabaseManager;
+import com.example.eaeprojekt.IngredientDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PopupIngredients implements AdapterView.OnItemSelectedListener, View.OnClickListener {
@@ -27,18 +32,31 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
     View popupView;
     PopupWindow popupWindow;
     //Adapter für Zutaten
-    ArrayAdapter<CharSequence> adapterIngredients;
+    Spinner ingredients;
+    ArrayAdapter<String> adapterIngredients;
 
     FrameLayout frame;
+
+    ArrayList<String> ingredientList;
     
     
     ConstraintLayout cancelButtonIngredient;
+
+    EditText nameText;
+    EditText unitText;
+    ConstraintLayout createIngredient;
+
 
     ImageView addIngredientCross;
 
     public void showPopupWindow(final View view, NewRecipeActivity newRecipeActivity) {
 
+
         mainActivity = newRecipeActivity;
+
+        db = new DatabaseManager(mainActivity);
+        db.open();
+
 
         LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.add_ingredients_popup, null);
@@ -56,13 +74,35 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
         //Spinner Zutaten
-        Spinner ingredients = popupView.findViewById(R.id.spinnerIngredient);
+        ingredients = popupView.findViewById(R.id.spinnerIngredient);
+
+/*
         adapterIngredients = ArrayAdapter.createFromResource(mainActivity, R.array.zutat, android.R.layout.simple_spinner_dropdown_item);
         ingredients.setAdapter(adapterIngredients);
+ */
+
+        //Spinner mit daten aus db füllen
+        ingredientList = new ArrayList<>();
+
+        List<IngredientDTO> allIngredients = db.getAllIngredients();
+        for(IngredientDTO newIngredient : allIngredients){
+            Log.d("spinner"," "+ newIngredient.getName() + " " + newIngredient.getUnit());
+            ingredientList.add(newIngredient.getName() + ", " + newIngredient.getUnit());
+            Log.d("spinner"," "+ newIngredient.getName() + " " + newIngredient.getUnit() + " wurde hinzugefügt");
+        }
+
+        adapterIngredients = new ArrayAdapter<>(mainActivity, android.R.layout.simple_spinner_dropdown_item, ingredientList);
+        ingredients.setAdapter(adapterIngredients);
+
+        //Spinner gefüllt
         ingredients.setOnItemSelectedListener(mainActivity);
+
 
         addIngredientCross = popupView.findViewById(R.id.addIngredientCross);
         addIngredientCross.setOnClickListener(this);
+
+        createIngredient = popupView.findViewById(R.id.createIngredient);
+        createIngredient.setOnClickListener(this);
         
         
         
@@ -73,8 +113,6 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-        db = new DatabaseManager(mainActivity);
-        db.open();
         if(adapterView.getItemAtPosition(position) == "neue Zutat erstellen"){
 
         }
@@ -91,16 +129,38 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
     public void onClick(View view) {
 
         frame = mainActivity.findViewById(R.id.mainmenu);
+        ConstraintLayout layoutAddIngredient = popupView.findViewById(R.id.addIngredientLayout);
 
         if (view == addIngredientCross){
-            ConstraintLayout layout = popupView.findViewById(R.id.addIngredientLayout);
 
-            if(layout.getVisibility() == View.VISIBLE){
-                layout.setVisibility(View.INVISIBLE);
+            if(layoutAddIngredient.getVisibility() == View.VISIBLE){
+                layoutAddIngredient.setVisibility(View.INVISIBLE);
             }else {
-                layout.setVisibility(View.VISIBLE);
+                layoutAddIngredient.setVisibility(View.VISIBLE);
             }
-        } else if (view == cancelButtonIngredient) {
+        }else if(view == createIngredient){
+            nameText = popupView.findViewById(R.id.nameText);
+            String nameTextString = nameText.getText().toString();
+
+            unitText = popupView.findViewById(R.id.unitText);
+            String unitTextString = unitText.getText().toString();
+
+            if(!nameTextString.isEmpty() && !unitTextString.isEmpty()){
+                db.insertIngredient(nameTextString, unitTextString);
+
+                ingredientList.add(nameTextString + ", " + unitTextString);
+                adapterIngredients.notifyDataSetChanged();
+
+                layoutAddIngredient.setVisibility(View.INVISIBLE);
+
+
+            }else{
+                Toast toast = new Toast(mainActivity);
+                toast.setText("Füllen sie zuerst die Felder aus");
+                toast.show();
+            }
+
+        }else if (view == cancelButtonIngredient) {
             frame.getForeground().setAlpha(0);
             popupWindow.dismiss();
         }
