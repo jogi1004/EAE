d1a75be9e7b6abb9f02c5e1f2b2fccf4c4126226
noeh1001 <noeh1001@stud.hs-kl.de>
2023-10-23 11:52:26 +0200

    Zutaten werden in db geschrieben

--- app/src/main/java/com/example/eaeprojekt/PopupIngredients.java
+++ app/src/main/java/com/example/eaeprojekt/PopupIngredients.java
@@ -1,9 +1,11 @@
 package com.example.eaeprojekt;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,9 +15,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.example.eaeprojekt.activity.NewRecipeActivity;
 import com.example.eaeprojekt.database.DatabaseManager;
@@ -41,6 +45,7 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
     
     
     ConstraintLayout cancelButtonIngredient;
+    ConstraintLayout addButtonIngredient;
 
     EditText nameText;
     EditText unitText;
@@ -109,13 +114,14 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
         cancelButtonIngredient = popupView.findViewById(R.id.cancel_button_ingredient);
         cancelButtonIngredient.setOnClickListener(this);
 
+        addButtonIngredient = popupView.findViewById(R.id.add_button_ingredient);
+        addButtonIngredient.setOnClickListener(this);
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-        if(adapterView.getItemAtPosition(position) == "neue Zutat erstellen"){
-
-        }
+        Log.d("Was", "Hallo? " + adapterView.getItemAtPosition(position));
 
 
     }
@@ -131,6 +137,12 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
         frame = mainActivity.findViewById(R.id.mainmenu);
         ConstraintLayout layoutAddIngredient = popupView.findViewById(R.id.addIngredientLayout);
 
+        nameText = popupView.findViewById(R.id.nameText);
+        String nameTextString = nameText.getText().toString();
+
+        unitText = popupView.findViewById(R.id.unitText);
+        String unitTextString = unitText.getText().toString();
+
         if (view == addIngredientCross){
 
             if(layoutAddIngredient.getVisibility() == View.VISIBLE){
@@ -139,11 +151,6 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
                 layoutAddIngredient.setVisibility(View.VISIBLE);
             }
         }else if(view == createIngredient){
-            nameText = popupView.findViewById(R.id.nameText);
-            String nameTextString = nameText.getText().toString();
-
-            unitText = popupView.findViewById(R.id.unitText);
-            String unitTextString = unitText.getText().toString();
 
             if(!nameTextString.isEmpty() && !unitTextString.isEmpty()){
                 db.insertIngredient(nameTextString, unitTextString);
@@ -163,6 +170,81 @@ public class PopupIngredients implements AdapterView.OnItemSelectedListener, Vie
         }else if (view == cancelButtonIngredient) {
             frame.getForeground().setAlpha(0);
             popupWindow.dismiss();
+        } else if (view == addButtonIngredient) {
+
+            frame.getForeground().setAlpha(0);
+            popupWindow.dismiss();
+
+            //TODO
+
+            long ingredientId = db.insertIngredientQuantity(NewRecipeActivity.newRecipeId, );
+
+            //schrittbeschreibung in der view hinzufügen
+
+            ConstraintLayout layout = new ConstraintLayout(mainActivity);
+
+            ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+            );
+            layout.setBackgroundResource(R.drawable.background_with_rounded_corners_green);
+            layout.setPadding(20,20,20,20);
+            layout.setLayoutParams(layoutParams);
+            layoutParams.setMargins(40, 10, 40, 10);
+
+            // Text der Schrittbeschreibung
+            TextView stepDescriptionText = new TextView(mainActivity);
+            stepDescriptionText.setId(View.generateViewId());
+            stepDescriptionText.setText(nameTextString);
+            stepDescriptionText.setGravity(Gravity.CENTER);
+            stepDescriptionText.setTextColor(Color.parseColor("#FFFFFF"));
+
+            ViewGroup.LayoutParams textViewParams = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+            stepDescriptionText.setLayoutParams(textViewParams);
+
+            layout.addView(stepDescriptionText);
+
+            //Mülleimer
+            ImageView trash = new ImageView(mainActivity);
+            trash.setImageResource(R.drawable.light_trash_can);
+            trash.setId(View.generateViewId());
+
+            ViewGroup.LayoutParams trashParams = new ViewGroup.LayoutParams(
+                    70,
+                    70
+            );
+            trash.setLayoutParams(trashParams);
+            layout.addView(trash);
+
+
+            //Constraints
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(layout);
+
+            constraintSet.connect(stepDescriptionText.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+            constraintSet.connect(stepDescriptionText.getId(), ConstraintSet.END, trash.getId(), ConstraintSet.START);
+            constraintSet.connect(stepDescriptionText.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+            constraintSet.connect(stepDescriptionText.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+
+
+            constraintSet.connect(trash.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+            constraintSet.connect(trash.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+            constraintSet.connect(trash.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+
+            constraintSet.applyTo(layout);
+
+
+            LinearLayout parentLayout = mainActivity.findViewById(R.id.stepsLayout);
+            parentLayout.addView(layout);
+
+            trash.setOnClickListener(v ->{
+                db.deleteStep(ingredientId);
+                parentLayout.removeView(layout);
+            });
+
         }
     }
 }
